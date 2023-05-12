88b2b569fb0a1bcb36764accec3793b81e6216dc
jofa446 <123947876+jofa446@users.noreply.github.com>
2023-05-12 10:17:06 +0200

    Add files via upload

---
+++ M05UF2_Examen_Joel/src/main/java/com/mycompany/m05uf2_examen_joel/Suma.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.m05uf2_examen_joel;
+
+/**
+ *
+ * @author alumne
+ */
+public class Suma {
+    private int num1;
+    private int num2;
+
+    public Suma(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+
+
+    public int getNum1() {
+        return num1;
+    }
+
+
+}
+
